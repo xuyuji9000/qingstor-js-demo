4bbd58f9173bbc51f4de999ce5c73ef27224a08e
徐余吉 <yogiman@xuyujis-MacBook-Pro.local>
2018-03-14 16:36:02 +0800

    add feature: list objects

--- Readme.md
+++ Readme.md
@@ -3,7 +3,7 @@
 ```
 export QINGCLOUD_ACCESS_KEY_ID=accesskeyid
 export QINGCLOUD_SECRET_ACCESS_KEY=secretaccesskey
-export QINGCLOUD_LOCATION=sha1
+export QINGCLOUD_LOCATION=sh1a
 ```
 
 # Install Dependencies
--- index.js
+++ index.js
@@ -3,13 +3,17 @@ let Config = require('qingstor-sdk').Config
 
 if(!process.env.QINGCLOUD_ACCESS_KEY_ID || !process.env.QINGCLOUD_SECRET_ACCESS_KEY || !process.env.QINGCLOUD_LOCATION) {
     console.log('Error, checkout qingcloud configuration in Readme.')
-    return
+    // return
 }
 
 let config = new Config(process.env.QINGCLOUD_ACCESS_KEY_ID, process.env.QINGCLOUD_SECRET_ACCESS_KEY)
 
 let service = new QingStor(config)
-
 service.listBuckets({'location': process.env.QINGCLOUD_LOCATION}, function(err, data){
   console.log(data.buckets);
+})
+
+let bucket = service.Bucket('face-detection', process.env.QINGCLOUD_LOCATION)
+bucket.listObjects({}, function (err, data) {
+    console.log(data.keys)
 })
