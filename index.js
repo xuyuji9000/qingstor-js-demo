a870cad86948cf6e6553fe766504bf2bb2266a96
徐余吉 <yogiman@xuyujis-MacBook-Pro.local>
2018-03-14 18:43:18 +0800

    add feature: add object to qingstor

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let QingStor = require('qingstor-sdk').QingStor
 let Config = require('qingstor-sdk').Config
+const fs = require('fs')
 
 if(!process.env.QINGCLOUD_ACCESS_KEY_ID || !process.env.QINGCLOUD_SECRET_ACCESS_KEY || !process.env.QINGCLOUD_LOCATION) {
     console.log('Error, checkout qingcloud configuration in Readme.')
@@ -15,5 +16,13 @@ service.listBuckets({'location': process.env.QINGCLOUD_LOCATION}, function(err, 
 
 let bucket = service.Bucket('face-detection', process.env.QINGCLOUD_LOCATION)
 bucket.listObjects({}, function (err, data) {
-    console.log(data.keys)
+    console.log(data.keys.length)
+})
+
+
+// console.log(bucket.putObject)
+bucket.putObject(new Date().getTime().toString(), {
+    'body': fs.createReadStream('./Readme.md')
+}, function(err, res) {
+  console.log(res.statusCode);
 })
